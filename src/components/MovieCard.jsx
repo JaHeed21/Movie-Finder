d3f08fa049cd398784a5c3fe1c3e2697ff46167a
JaHeed21 <jahidulrayhan2002@gmail.com>
2025-07-17 19:47:38 +0600

    display the movie poster with title

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({movie:
+    {title,id, vote_average, poster_path, release_date, original_language} 
+}) => {
   return (
-    <div>
-        <p className="text-white">{movie.title}</p>
+    <div className='movie-card'>
+        <img src={poster_path? `https://image.tmdb.org/t/p/w500/${poster_path}` : 
+        '/no-movie.png'} alt="" />
+        <p  key={id} className="text-white">{title}</p>
     </div>
   )
 }
